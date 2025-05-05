f8944b7a083db6b8f5eb18f7fb09b90198350030
Dilemma98 <emmaah98@outlook.com>
2025-05-05 13:49:01 +0200

    angular

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c83edf9c56b170db527979e5272e2f7ba076ec3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8c3b9d199c8d908da4cca99edb1e1f121bb1025")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
